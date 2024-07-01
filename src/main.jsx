290874ea1ca3b04e535c41b936c201530d24a558
josé <jg345762@gmail.com>
2024-07-01 19:02:08 -0300

    src/main.jsx updated

--- src/main.jsx
+++ src/main.jsx
@@ -7,26 +7,31 @@ import TopicosRoute from "./routes/TopicosRoute";
 import CrearTopicoRoute from "./routes/CrearTopicoRoute";
 import { TopicosContextProvider } from "./context/TopicosContextProvider";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/topicos",
+          element: <TopicosRoute />,
+        },
+        {
+          path: "/crear-topico",
+          element: <CrearTopicoRoute />,
+        },
+        {
+          path: "/crear-topico/:id",
+          element: <CrearTopicoRoute />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/topicos",
-        element: <TopicosRoute />,
-      },
-      {
-        path: "/crear-topico",
-        element: <CrearTopicoRoute />,
-      },
-      {
-        path: "/crear-topico/:id",
-        element: <CrearTopicoRoute />,
-      },
-    ],
-  },
-]);
+    basename: "",
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
